1c272365e173c4218c6fdb03aea0a09c56a0e181
Carrington <hartis.c99@gmail.com>
2022-04-05 18:49:24 -0400

    CRUD for posts

--- controllers/post-route.js
+++ controllers/post-route.js
@@ -5,8 +5,55 @@ const { Post } = require('../models');
 router.get('/post', async (req, res) => {
   try {
     const postData = await Post.findAll()
-    console.log(postData);
     res.json(postData);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+// Get one post by ID.
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id);
+    res.json(postData);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Create a new post.
+router.post('/post', async (req, res) => {
+  try {
+    const postData = await Post.create(req.body);
+    res.json(postData);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+// Update a post by ID.
+router.put('/post/:id', async (req, res) => {
+  try {
+    const updatedPost = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+    res.json(updatedPost);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+// Delete post by ID.
+router.delete('/post/:id', async (req, res) => {
+  try {
+    const deletedPost = await Post.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    res.json(deletedPost);
   } catch (err) {
     res.json(err);
   }
