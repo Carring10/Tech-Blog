0023b961f2a744f24c52cd8b15ab78a83599d78a
Carrington <hartis.c99@gmail.com>
2022-04-07 22:51:55 -0400

    dashboard renders posts

---
+++ controllers/dashboard.js
@@ -0,0 +1,68 @@
+const router = require('express').Router();
+const { Post } = require('../models');
+
+// Get all posts.
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll();
+    // Loop through each post, turning them into a plain obj
+    const posts = postData.map((post) => post.get({ plain: true }));
+    // to have them all rendered to the landing page template with handlebars
+    console.log(posts)
+    res.render('dashboard', { posts });
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+// Get one post by ID.
+router.get('/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id);
+    res.json(postData);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Create a new post.
+router.post('/', async (req, res) => {
+  try {
+    const postData = await Post.create(req.body);
+    res.json(postData);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+// Update a post by ID.
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedPost = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+    res.json(updatedPost);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+// Delete post by ID.
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedPost = await Post.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    res.json(deletedPost);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+
+
+module.exports = router;
--- controllers/homepage-route.js
+++ controllers/homepage-route.js
@@ -26,54 +26,11 @@ router.get('/login', (req, res) => {
   res.render('login', { loggedIn: req.session.loggedIn });
 });
 
-// Get one post by ID.
-router.get('/:id', async (req, res) => {
-  try {
-    const postData = await Post.findByPk(req.params.id);
-    res.json(postData);
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-// Create a new post.
-router.post('/', async (req, res) => {
-  try {
-    const postData = await Post.create(req.body);
-    res.json(postData);
-  } catch (err) {
-    res.json(err);
-  }
-});
-
-// Update a post by ID.
-router.put('/:id', async (req, res) => {
-  try {
-    const updatedPost = await Post.update(req.body, {
-      where: {
-        id: req.params.id,
-      }
-    });
-    res.json(updatedPost);
-  } catch (err) {
-    res.json(err);
-  }
-});
-
-// Delete post by ID.
-router.delete('/:id', async (req, res) => {
-  try {
-    const deletedPost = await Post.destroy({
-      where: {
-        id: req.params.id,
-      }
-    });
-    res.json(deletedPost);
-  } catch (err) {
-    res.json(err);
-  }
-});
-
+// // dashboard
+// router.get('/dashboard', (req, res) => {
+//   // Render handlebars dashboard page.
+//   res.render('dashboard', { loggedIn: req.session.loggedIn });
+// });
 
 
 module.exports = router;
